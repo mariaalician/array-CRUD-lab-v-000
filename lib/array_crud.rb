b1f738f9d167b193ea6899826010e356ea1dd1df
Maria Nunez <mariaalician@gmail.com>
2018-06-11 22:15:46 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,19 +11,19 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  array.unshift
+  array.unshift(1)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array.pop(1)
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array.shift(1)
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array[2]
 end
 
 def retrieve_first_element_from_array(array)
